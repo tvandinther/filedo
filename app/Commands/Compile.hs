34845b38a1f5d47ba8188b44835a774ed64336cf
Tom van Dinther <tom.vandinther@myob.com>
2022-11-27 11:23:34 +1300

    add options

--- app/Commands/Compile.hs
+++ app/Commands/Compile.hs
@@ -5,7 +5,9 @@ module Commands.Compile (
 import Options.Applicative
 
 data CompileOptions = CompileOptions
-    { outputFile' :: Maybe FilePath }
+    { templateFiles :: [FilePath]
+    , dataFiles :: [FilePath]
+    , outputDirectory :: FilePath }
     deriving (Show)
 
 compileInfo :: ParserInfo CompileOptions
@@ -14,4 +16,26 @@ compileInfo = info compileOptions (progDesc "Compile mustache templates")
 compileOptions :: Parser CompileOptions
 compileOptions =
     CompileOptions <$>
-    optional (strOption (long "output" <> short 'o' <> metavar "OUTPUT"))
+    templateFilesArgument
+    <*> dataFilesOption
+    <*> outputDirectoryOption
+
+templateFilesArgument :: Parser [FilePath]
+templateFilesArgument = some (argument str 
+    ( metavar "TEMPLATEFILES..." 
+    <> help "Mustache templates." ))
+
+dataFilesOption :: Parser [FilePath]
+dataFilesOption = some (strOption 
+    ( long "data" 
+    <> short 'd' 
+    <> metavar "DATAFILES..." 
+    <> help "Data files to use. Last argument takes merge precedence." ))
+
+outputDirectoryOption :: Parser FilePath
+outputDirectoryOption = strOption 
+    ( long "output" 
+    <> short 'o' 
+    <> metavar "OUTPUTDIR" 
+    <> value "_build"
+    <> help "Directory for compiled template output." )
--- app/Commands/MergeData.hs
+++ app/Commands/MergeData.hs
@@ -4,10 +4,13 @@ module Commands.MergeData (
 ) where
 
 import Options.Applicative
+import Data( DataFileType(..) )
+import Data.Char (toLower)
 
 data MergeDataOptions = MergeDataOptions
     { dataFiles :: [FilePath]
-    , outputFile :: Maybe FilePath }
+    , outputFile :: Maybe FilePath
+    , outputType :: DataFileType }
     deriving (Show)
 
 mergeDataInfo :: ParserInfo MergeDataOptions
@@ -16,5 +19,32 @@ mergeDataInfo = info mergeDataOptions (progDesc "Merge data files")
 mergeDataOptions :: Parser MergeDataOptions
 mergeDataOptions =
     MergeDataOptions <$>
-    many (argument str (metavar "DATAFILES..."))
-    <*> optional (strOption (long "output" <> short 'o' <> metavar "OUTPUT"))
+    dataFilesArgument
+    <*> outputFileOption
+    <*> outputTypeOption
+
+dataFilesArgument :: Parser [FilePath]
+dataFilesArgument = some (argument str 
+    ( metavar "DATAFILES..." 
+    <> help "Data files to merge. Last argument takes merge precedence." ))
+
+outputFileOption :: Parser (Maybe FilePath)
+outputFileOption = optional (strOption 
+    ( long "output" 
+    <> short 'o' 
+    <> metavar "OUTPUT"
+    <> help "Output file for merged data." ))
+
+outputTypeOption :: Parser DataFileType
+outputTypeOption = option readOutputType
+    ( long "type"
+    <> short 't'
+    <> metavar "TYPE"
+    <> value JSON
+    <> help "Output type for merged data. (json, yaml)" )
+
+readOutputType :: ReadM DataFileType
+readOutputType = str >>= \s -> case map toLower s of
+    "json" -> return JSON
+    "yaml" -> return YAML
+    _ -> readerError $ "Cannot parse value '" ++ s ++ "'. Expecting one of: json, yaml"
