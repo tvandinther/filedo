a559875362694462d0d9bec54ae96491fdbc2504
Tom van Dinther <39470469+tvandinther@users.noreply.github.com>
2023-08-12 17:12:50 +0200

    Update examples

--- examples/deploy.sh
+++
@@ -1,8 +0,0 @@
-# Finds a config.gal in ./templates and runs through the rules. 
-# Uses the data files to populate mustache templated values with the last argument taking precedence.
-# More or less like `kt deploy -e base -e callisto-dev`
-app magic ./templates --data env/base --data env/callisto-dev
-
-stack run -- merge-data testfiles/env/callisto-dev.yaml testfiles/test.json -t yaml | stack run -- compile testfiles
-
-stack run -- compile testfiles -d testfiles/env/callisto-dev.yaml -d testfiles/test.json
