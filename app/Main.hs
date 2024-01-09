e161d400943bf96b72a0aa02bf297493f41255c6
Tom van Dinther <39470469+tvandinther@users.noreply.github.com>
2024-01-09 09:42:28 +0100

    Update options parsing in Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -2,15 +2,17 @@ module Main (main) where
 
 import Commands (Command (..))
 import Options (GlobalOptions (..), optsParser)
-import Options.Applicative (execParser)
+import Options.Applicative
 import Runners.Compile (runCompile)
 import Runners.MergeData (runMergeData)
 import Runners.Process (runProcess)
 
 main :: IO ()
 main = do
-  opts <- execParser optsParser
+  opts <- customExecParser customPrefs optsParser
   run opts (optCommand opts)
+  where
+    customPrefs = prefs $ showHelpOnError <> showHelpOnEmpty <> disambiguate
 
 run :: GlobalOptions -> Command -> IO ()
 run _ (MergeData o) = runMergeData o
